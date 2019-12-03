5e8fda08f3b0ffa726160ff677d7903c859d0664
Eric Andrews <ejandre@umich.edu>
2019-12-03 16:51:31 -0500

    Packages and a couple function plans

--- Final_Proj_Retrieve_Script.py
+++ Final_Proj_Retrieve_Script.py
@@ -2,3 +2,16 @@ import json
 import requests
 import csv
 import spotipy
+import os
+import sqlite3
+import spotipy.util as util
+from bs4 import BeautifulSoup
+
+def grab_spotify_top_tracks():
+    pass
+
+
+
+
+def get_song_info():
+    pass
