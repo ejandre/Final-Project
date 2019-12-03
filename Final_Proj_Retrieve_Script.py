643edab028a7dcefd7076576d92e04f4a2a1e9b1
Eric Andrews <ejandre@umich.edu>
2019-12-03 17:24:19 -0500

    Working on getting Access Token

--- Final_Proj_Retrieve_Script.py
+++ Final_Proj_Retrieve_Script.py
@@ -8,7 +8,14 @@ import spotipy.util as util
 from bs4 import BeautifulSoup
 
 def grab_spotify_top_tracks():
-    pass
+    token = util.prompt_for_user_token(username ='Ejandre',scope='user-top-read', client_id='3b54d53b0c474780af4fa86c0242364b',client_secret='42c07ded757a42d69d12beef09424504',redirect_uri='https://accounts.spotify.com/authorize')
+    if token:
+        sp_obj = spotipy.Spotify()
+        results = sp_obj.current_user_top_tracks(time_range='medium_term',limit = 20)
+        print(results)
+
+
+grab_spotify_top_tracks()
 
 
 
