e8ba609df635e47d39b4dc00a994ac3f59731d33
Eric Andrews <ejandre@umich.edu>
2019-12-03 21:06:17 -0500

    Spotify Top Tracks is now returned as a dictionary of Songs as Keys and Artists as values

--- Final_Proj_Retrieve_Script.py
+++ Final_Proj_Retrieve_Script.py
@@ -7,18 +7,21 @@ import sqlite3
 import spotipy.util as util
 from bs4 import BeautifulSoup
 
-def grab_spotify_top_tracks():
-    token = util.prompt_for_user_token(username ='Ejandre',scope='user-top-read', client_id='3b54d53b0c474780af4fa86c0242364b',client_secret='42c07ded757a42d69d12beef09424504',redirect_uri='https://accounts.spotify.com/authorize')
-
+def grab_spotify_top_tracks(username,scope,client_id,client_secret,redirect_uri):
+    song_artist = {}
+    #token = util.prompt_for_user_token(username ='Ejandre',scope='user-top-read', client_id='3b54d53b0c474780af4fa86c0242364b',client_secret='42c07ded757a42d69d12beef09424504',redirect_uri='https://accounts.spotify.com/authorize')
+    token = util.prompt_for_user_token(username,scope, client_id,client_secret,redirect_uri)
     if token:
         sp_obj = spotipy.Spotify(token)
         results = sp_obj.current_user_top_tracks(time_range='medium_term',limit = 20)
         for name in results['items']:
-            print(name['name'] + " " + name['artists'][0]['name'])
+            song_artist[name['name']] = name['artists'][0]['name']
+        print(song_artist)
+    return song_artist
 
 
-grab_spotify_top_tracks()
-
+eric = grab_spotify_top_tracks('Ejandre','user-top-read','3b54d53b0c474780af4fa86c0242364b','42c07ded757a42d69d12beef09424504','https://accounts.spotify.com/authorize')
+#shanley =  grab_spotify_top_tracks('','','','','https://accounts.spotify.com/authorize')
 
 
 
