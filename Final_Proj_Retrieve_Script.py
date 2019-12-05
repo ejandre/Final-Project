520de8d91273d12aacf88415e6f6a6bc9c6d1c23
Eric Andrews <ejandre@umich.edu>
2019-12-05 09:39:14 -0500

    Working, but only returning dictonary for last word

--- Final_Proj_Retrieve_Script.py
+++ Final_Proj_Retrieve_Script.py
@@ -15,7 +15,7 @@ def grab_spotify_top_tracks(username,scope,client_id,client_secret,redirect_uri)
         results = sp_obj.current_user_top_tracks(time_range='medium_term',limit = 20)
         for name in results['items']:
             song_artist.append((name['name'], name['artists'][0]['name']))
-        print(song_artist)
+    print(song_artist)
     return song_artist
 
 
@@ -28,16 +28,16 @@ def get_song_lyrics(song,artist):
     gen_obj = lyricsgenius.Genius("uwm7hKHlSCzTd6jsJVxKx0CGv_TumqMn-YK4Eh1WNBkwAEYz5tbWSU31iYfrQaOO")
     song = gen_obj.search_song(song, artist)
     lyrics = song.lyrics
-    print(lyrics)
     return lyrics
 
+
 def write_lyrics_to_txt(lyrics,filename):
-    f = open(filename, "w")
+    f = open(filename, "w", encoding='utf8')
     f.write(lyrics)
 
 def check_frequency_of_words(text):
     freq = {}
-    read_txt = open(text, "r")
+    read_txt = open(text, "r", encoding = 'utf8')
     for word in read_txt.readlines():
         word = word.lower()
         for w in word.split():
@@ -45,19 +45,22 @@ def check_frequency_of_words(text):
                 freq[w] = 1
             else:
                 freq[w] += 1
-    return freq
+    return sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
     
 
 
     
-
-
-def main():
-    pass
 
 '''
+def main():
+    eric = grab_spotify_top_tracks('Ejandre','user-top-read','3b54d53b0c474780af4fa86c0242364b','42c07ded757a42d69d12beef09424504','https://accounts.spotify.com/authorize')
+    for x in eric:
+        lyr = get_song_lyrics(x[0],x[1])
+        write_lyrics_to_txt(lyr, "lyrics.txt")
+    print(check_frequency_of_words("lyrics.txt"))
+
+
 if __name__ == "__main__":
     main()
-    '''
-
+'''
