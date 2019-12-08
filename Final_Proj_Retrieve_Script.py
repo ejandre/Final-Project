3c1130a0a584b96de95cff121a37e4a74422f612
Eric Andrews <ejandre@umich.edu>
2019-12-08 13:38:54 -0500

    Working, weird returns sometimes

--- Final_Proj_Retrieve_Script.py
+++ Final_Proj_Retrieve_Script.py
@@ -12,27 +12,32 @@ def grab_spotify_top_tracks(username,scope,client_id,client_secret,redirect_uri)
     token = util.prompt_for_user_token(username,scope, client_id,client_secret,redirect_uri)
     if token:
         sp_obj = spotipy.Spotify(token)
-        results = sp_obj.current_user_top_tracks(time_range='medium_term',limit = 20)
+        results = sp_obj.current_user_top_tracks(time_range='short_term',limit = 20)
         for name in results['items']:
             song_artist.append((name['name'], name['artists'][0]['name']))
+            #if name['name'] == "Frankie Valli":
+             #   song_artist.remove("Fallen Angel","Frankie Valli")
     print(song_artist)
     return song_artist
 
 
-eric = grab_spotify_top_tracks('Ejandre','user-top-read','3b54d53b0c474780af4fa86c0242364b','42c07ded757a42d69d12beef09424504','https://accounts.spotify.com/authorize')
+#eric = grab_spotify_top_tracks('Ejandre','user-top-read','3b54d53b0c474780af4fa86c0242364b','42c07ded757a42d69d12beef09424504','https://accounts.spotify.com/authorize')
 #shanley =  grab_spotify_top_tracks('','','','','https://accounts.spotify.com/authorize')
 
 
 
 def get_song_lyrics(song,artist):
     gen_obj = lyricsgenius.Genius("uwm7hKHlSCzTd6jsJVxKx0CGv_TumqMn-YK4Eh1WNBkwAEYz5tbWSU31iYfrQaOO")
+    gen_obj.remove_section_headers = True
+    gen_obj.skip_non_songs = True
+    gen_obj.excluded_terms = ["brumberg"]
     song = gen_obj.search_song(song, artist)
     lyrics = song.lyrics
     return lyrics
 
 
 def write_lyrics_to_txt(lyrics,filename):
-    f = open(filename, "w", encoding='utf8')
+    f = open(filename, "a+", encoding='utf8')
     f.write(lyrics)
 
 def check_frequency_of_words(text):
@@ -52,15 +57,21 @@ def check_frequency_of_words(text):
 
     
 
-'''
+
 def main():
     eric = grab_spotify_top_tracks('Ejandre','user-top-read','3b54d53b0c474780af4fa86c0242364b','42c07ded757a42d69d12beef09424504','https://accounts.spotify.com/authorize')
+
     for x in eric:
-        lyr = get_song_lyrics(x[0],x[1])
+        try:
+            lyr = get_song_lyrics(x[0],x[1])
+        except:
+            continue
+
+        #print(lyr)
         write_lyrics_to_txt(lyr, "lyrics.txt")
-    print(check_frequency_of_words("lyrics.txt"))
+        print(check_frequency_of_words("lyrics.txt"))
 
 
 if __name__ == "__main__":
     main()
-'''
+
