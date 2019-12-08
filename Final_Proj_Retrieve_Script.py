74d35093707c2a7ca726de62184c1ee89f37591b
Eric Andrews <ejandre@umich.edu>
2019-12-08 14:42:18 -0500

    Code Successfully retrieving frequency of song lyrics

--- Final_Proj_Retrieve_Script.py
+++ Final_Proj_Retrieve_Script.py
@@ -12,11 +12,11 @@ def grab_spotify_top_tracks(username,scope,client_id,client_secret,redirect_uri)
     token = util.prompt_for_user_token(username,scope, client_id,client_secret,redirect_uri)
     if token:
         sp_obj = spotipy.Spotify(token)
-        results = sp_obj.current_user_top_tracks(time_range='short_term',limit = 20)
+        results = sp_obj.current_user_top_tracks(time_range='medium_term',limit = 20)
         for name in results['items']:
             song_artist.append((name['name'], name['artists'][0]['name']))
-            #if name['name'] == "Frankie Valli":
-             #   song_artist.remove("Fallen Angel","Frankie Valli")
+            if name['name'] == "Fallen Angel":
+                song_artist.remove(("Fallen Angel","Frankie Valli"))
     print(song_artist)
     return song_artist
 
@@ -30,15 +30,15 @@ def get_song_lyrics(song,artist):
     gen_obj = lyricsgenius.Genius("uwm7hKHlSCzTd6jsJVxKx0CGv_TumqMn-YK4Eh1WNBkwAEYz5tbWSU31iYfrQaOO")
     gen_obj.remove_section_headers = True
     gen_obj.skip_non_songs = True
-    gen_obj.excluded_terms = ["brumberg"]
     song = gen_obj.search_song(song, artist)
     lyrics = song.lyrics
     return lyrics
 
 
 def write_lyrics_to_txt(lyrics,filename):
-    f = open(filename, "a+", encoding='utf8')
+    f = open(filename, "a", encoding='utf8')
     f.write(lyrics)
+    f.close()
 
 def check_frequency_of_words(text):
     freq = {}
@@ -67,9 +67,8 @@ def main():
         except:
             continue
 
-        #print(lyr)
         write_lyrics_to_txt(lyr, "lyrics.txt")
-        print(check_frequency_of_words("lyrics.txt"))
+    print(check_frequency_of_words("lyrics.txt"))
 
 
 if __name__ == "__main__":
